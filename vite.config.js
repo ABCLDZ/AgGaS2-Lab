5a6d4989e929a526d77033a18da4b89c1731eabc
ABCLDZ <zhenyuluo1031@gmail.com>
2026-01-13 17:10:24 +0800

    chore: 配置部署参数和 base 路径

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vite.js.dev/config/
 export default defineConfig({
+  // 关键：添加这一行，确保打包后的资源路径指向你的仓库名
+  base: '/AgGaS2-Lab/', 
   plugins: [react()],
 })
